8913e960678311ca3dffc8aa4b95d0ff2dc6396c
BlackCc97 <1203825081@qq.com>
2026-07-22 11:24:27 +0800

    Update visit assistant prototype

--- payload-10.js
+++
@@ -1,1 +0,0 @@
-window.__WATSONS_PAYLOAD__=(window.__WATSONS_PAYLOAD__||'')+'7NaatwGmqrljhBzcdMcb49/BjWpb23JP1PiTNkXitraO4kinmXgf6E5wwXh6A7ATs6rVDsnxl2utWDXytUNHnw7qfb8XHsOYiRhE7xCLonfo/wF+YPc4slUDAA==';
